e9e08c59bcfdbba276bf2803dc64c66a8927fcad
LAPTOP-EKA7766I\User <rebornharu12345@gmail.com>
2022-02-02 21:10:59 +0700

    create api /home

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-aaaa
-bbbbb
+const express  = require("express");
+
+const app = express()
+const port = 3000
+
+app.listen(port,()=>{
+    console.log(`start http://localhost:${port}`)
+})
+
+app.get('/home',(req,res) =>{
+    res.send('Hello Home page')
+})
+
+
